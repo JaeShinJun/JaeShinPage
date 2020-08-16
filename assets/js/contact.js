e68d40728905c4e292335b3f64d7c0ed9ced4c2c
JaeShinJun <jsstar1207@gmail.com>
2020-08-17 05:38:40 +0900

    Modify contact message design and add events

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,6 +3,32 @@
     const inputName = contactForm.querySelector(".form__name");
     const inputEmail = contactForm.querySelector(".form__email");
     const textareaMessage = contactForm.querySelector(".form__message");
+    const successDiv = document.querySelector(".success");
+    const successMessage = successDiv.querySelector(".success__message");
+    const successMask = successDiv.querySelector(".success__mask");
+    const successMessageExitBtn = successDiv.querySelector(
+        ".message__exit-btn"
+    );
+
+    const submitSuccessHandle = () => {
+        successDiv.setAttribute("style", "display: flex;");
+        successDiv.classList.add("show");
+
+        successMessage.classList.add("show");
+    };
+
+    const successMessageExitHandle = () => {
+        successDiv.classList.remove("show");
+        successDiv.classList.add("hide");
+
+        successMessage.classList.remove("show");
+        successMessage.classList.add("hide");
+
+        setTimeout(() => {
+            successDiv.classList.remove("hide");
+            successMessage.classList.remove("hide");
+        }, 1000);
+    };
 
     const contactFormSubmitHandle = async (event) => {
         event.preventDefault();
@@ -17,14 +43,22 @@
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(data),
-        }).then((res) => {
-            return res.json();
-        });
-        console.log(result);
+        })
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+                submitSuccessHandle();
+            })
+            .catch((error) => console.error("Error:", error));
     };
 
     const init = () => {
         contactForm.addEventListener("submit", contactFormSubmitHandle);
+        successMask.addEventListener("click", successMessageExitHandle);
+        successMessageExitBtn.addEventListener(
+            "click",
+            successMessageExitHandle
+        );
     };
 
     init();
