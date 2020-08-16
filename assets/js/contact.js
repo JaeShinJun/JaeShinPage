7ae0fa401916af8da5f0155ed932b201719ac3b3
JaeShinJun <jsstar1207@gmail.com>
2020-08-17 02:06:33 +0900

    Add contact form

---
+++ assets/js/contact.js
@@ -0,0 +1,32 @@
+{
+    const contactForm = document.querySelector(".contact-form");
+    const inputName = contactForm.querySelector(".form__name");
+    const inputEmail = contactForm.querySelector(".form__email");
+    const textareaMessage = contactForm.querySelector(".form__message");
+
+    const contactFormSubmitHandle = async (event) => {
+        event.preventDefault();
+        const data = {
+            name: inputName.value,
+            email: inputEmail.value,
+            message: textareaMessage.value,
+        };
+        const result = await fetch("https://formspree.io/xaypkgdj", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Referrer-Policy": "no-referrer",
+            },
+            body: { message: JSON.stringify(data) },
+        }).then((res) => {
+            return res.json();
+        });
+        console.log(result);
+    };
+
+    const init = () => {
+        contactForm.addEventListener("submit", contactFormSubmitHandle);
+    };
+
+    init();
+}
