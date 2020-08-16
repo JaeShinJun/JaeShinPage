fc10557432678ace834a2ff564c13a68efe01f20
JaeShinJun <jsstar1207@gmail.com>
2020-08-17 02:10:20 +0900

    Update submit handle for cors

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -16,6 +16,7 @@
             headers: {
                 "Content-Type": "application/json",
                 "Referrer-Policy": "no-referrer",
+                "Access-Control-Allow-Origin": "*",
             },
             body: { message: JSON.stringify(data) },
         }).then((res) => {
